de828d8f58079d7c0e06ad2dd278384c0bbee88b
Remy Martin Lagerweij <Remy@Remys-Mac-mini.local>
2016-10-08 21:56:55 +0200

    fix slick path typo

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 	<body>
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<script src="assets/js/min/vendor/slick-min.js"></script>
+		<script src="assets/js/vendor/min/slick-min.js"></script>
 		<script src="assets/js/min/application-min.js"></script>
 
 	</body>
